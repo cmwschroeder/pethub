531b8d6f49ce4842cca4090846c0d561a7de5b11
coderbennett <joeykb17@gmail.com>
2022-04-11 17:38:52 -0700

    add jumbotron, add search form criteria

---
+++ assets/petFinder.js
@@ -0,0 +1,14 @@
+var petFinderKey = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJzaXd5d2NIOHNtVnVZa1F3YVR4TGFVN281dWtYN3NrMkRKTkM4Vm15elFFcUVlQUJxOCIsImp0aSI6IjQ1MmRlMDFlYTEzMmVhYzBkM2E3ZDBkMzk1Y2I2ZDU2MjdkZTA3NDM5YzM3ODQyOWI3OGMwNDU1NDZhMWVmN2JlZjViYWQ1ZTM0NDZkMTA4IiwiaWF0IjoxNjQ5NzE4OTk0LCJuYmYiOjE2NDk3MTg5OTQsImV4cCI6MTY0OTcyMjU5NCwic3ViIjoiIiwic2NvcGVzIjpbXX0.pRT2q7ZpZ_8Rp_EjnisT4hOTWpQRryK4j-bWgFD7pQQZVx6mX0-wRWzO4W_hXhZEv6bUB6gTG2OJkZJUY-9ln6QmrRROZmqXHC98lsWqZ07cOJTtg1iZci4efbozRPpLttsNEdwP88zxBRlCwNSpFHfF-_c6RQjwgujV1URrzJooBIl36ZTbjLpY2v-7bBP_n_093iVEfImMyw7RdoD1JNP5AccSY2zILAfOeFSb_vAaM1ljqcBAbkeVZYdSi5kqz42MylbY9RxzsrQuSi36cNSBHZkxWg4WHWabsyjMHxSkaEN3Ljpi_KqQXf0rjtdJQs0xDKPbEoiXyMyiYkZHDQ';
+
+// fetch("https://api.petfinder.com/v2/animals", {
+//     method: 'GET',
+//     headers: new Headers({
+//         'Authorization': 'Bearer ' +  petFinderKey
+//     })
+// })
+// .then(function (response) {
+//   return response.json();
+// })
+// .then(function (data) {
+//   console.log(data);
+// });

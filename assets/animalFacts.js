2824f3d768a4857c21f453e2b7c1f1ca8907c5fe
kylie <kyliemshinn@gmail.com>
2022-04-12 18:59:30 -0700

    add outline for javascript

---
+++ assets/animalFacts.js
@@ -0,0 +1,17 @@
+//global variables
+
+//next two functions need to generate on page load
+
+//will need function to fetch api for cat facts
+
+//will need api to fetch dog facts
+
+//will want to store these facts to local storage
+
+//will need to dynamically generate card-like features to append the dog facts and pictures
+
+//decide between hard coding and  api generate pictures for animal pictures
+        //pros hard coding- can select which picture goes with which fact so it will go together better inflow
+        //cons- having a bunch of pictures generated for a bunch of facts
+
+//

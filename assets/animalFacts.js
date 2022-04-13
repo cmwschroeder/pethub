740b5025d2cb90d674db1bd1eb2dcaa7a02b80cd
kylie <kyliemshinn@gmail.com>
2022-04-13 09:17:14 -0700

    add API URls to fetch for facts in JS

--- assets/animalFacts.js
+++ assets/animalFacts.js
@@ -1,14 +1,47 @@
 //global variables
+var factCards = $("#fact-cards")
 
 //next two functions need to generate on page load
 
 //will need function to fetch api for cat facts
 
+function catFacts() {
+    fetch('https://cat-fact.herokuapp.com/facts')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
+
+}
+
+
 //will need api to fetch dog facts
+
+function dogFacts() {
+    fetch('https://dog-facts-api.herokuapp.com/api/v1/resources/dogs/all')
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+  
+
+}
 
 //will want to store these facts to local storage
 
+function storeFacts() {
+
+}
+
 //will need to dynamically generate card-like features to append the dog facts and pictures
+
+function displayFacts() {
+
+}
 
 //decide between hard coding and  api generate pictures for animal pictures
         //pros hard coding- can select which picture goes with which fact so it will go together better inflow
